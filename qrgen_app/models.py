7d69c2d46320997e8e13d5c08d2904060239d0d5
Gage Lieble <gagelieble@hotmail.com>
2022-08-15 15:44:52 -0700

    More Styling, Form Limitations set

--- qrgen_app/models.py
+++ qrgen_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class QrGenModel(models.Model):
-    qr_name = models.CharField(max_length=50)
+    qr_name = models.CharField(max_length=14)
     link = models.CharField(max_length=100)
     color = models.CharField(max_length=15)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
