735b7d71003685e44f5450ce3f702411774a41ef
Gage Lieble <gagelieble@hotmail.com>
2022-08-16 21:43:07 -0700

    Deployment settings

--- qrpixel_proj/settings.py
+++ qrpixel_proj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jgvu01+ccyxp__#*ub)s$9cd3-$c&k0dfllc-$%nqp9cohczc1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
