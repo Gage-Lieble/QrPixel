0e95ec2e0a9de1848cfe38059129631341f53d69
Gage Lieble <gagelieble@hotmail.com>
2022-08-16 21:42:21 -0700

    Deployment settings

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn qrpixel_proj.wsgi
+release: python manage.py migrate users && python manage.py migrate
--- qrpixel_proj/settings.py
+++ qrpixel_proj/settings.py
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
