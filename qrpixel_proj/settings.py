89378b919e27a7fe1a8324faa2cd659edec000c5
Gage Lieble <gagelieble@hotmail.com>
2022-08-16 21:45:49 -0700

    Deploy

--- qrpixel_proj/settings.py
+++ qrpixel_proj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "whitenoise.runserver_nostatic",
 
     'qrgen_app',
     'qruser_app',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
